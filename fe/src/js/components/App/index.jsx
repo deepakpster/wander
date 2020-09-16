254d5c73425bd6b148224d38cf2f780054907bf7
Deepak Puttarangaswamy (PD) <deepakpster@gmail.com>
2020-09-17 03:20:33 +0530

    fe: Add loading message in dashboard screen

--- fe/src/js/components/App/index.jsx
+++ fe/src/js/components/App/index.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import Delay from 'react-delay';
 import PropTypes from 'prop-types';
 
 export default class App extends React.Component {
@@ -11,26 +12,30 @@ export default class App extends React.Component {
       <nav>
         <button className="btn btn-secondary" onClick={this.props.authActions.logout}>Logout</button>
       </nav>
-      <div className="row">
+      {
+        (statistics.length > 0) ? <div className="row">
         {
           statistics.map((s,sIdx)=>{
             return <div key={`c-${sIdx}`} className="col-md-6 card">
                 <div className="card-header">{s.state_name || 'India (All States)'}</div>
-                <ul class="list-group list-group-flush">
-                <li class="list-group-item">Active(Total):{s.active}</li>
-                  <li class="list-group-item">Active(New):{s.new_active}</li>
-                  <li class="list-group-item">Positive(Total):{s.positive}</li>
-                  <li class="list-group-item">Positive(New):{s.new_positive}</li>
-                  <li class="list-group-item">Cured(Total):{s.cured}</li>
-                  <li class="list-group-item">Cured(New):{s.new_cured}</li>
-                  <li class="list-group-item">Death(Total):{s.death}</li>
-                  <li class="list-group-item">Death(New):{s.new_death}</li>
+                <ul className="list-group list-group-flush">
+                <li className="list-group-item">Active(Total):{s.active}</li>
+                  <li className="list-group-item">Active(New):{s.new_active}</li>
+                  <li className="list-group-item">Positive(Total):{s.positive}</li>
+                  <li className="list-group-item">Positive(New):{s.new_positive}</li>
+                  <li className="list-group-item">Cured(Total):{s.cured}</li>
+                  <li className="list-group-item">Cured(New):{s.new_cured}</li>
+                  <li className="list-group-item">Death(Total):{s.death}</li>
+                  <li className="list-group-item">Death(New):{s.new_death}</li>
                 </ul>
               </div>
           })
         }
-      </div>
-      </div>
-    );
+        </div> : <Delay wait={250}>
+          <p>Loading...</p>
+        </Delay>
+      }
+      
+    </div>);
   }
 }
